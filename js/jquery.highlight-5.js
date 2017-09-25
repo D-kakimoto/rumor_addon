036229832ab2b9e1d59a62a25f3c678a834c13ea
dkakimoto <shinra.dai0419@gmail.com>
2017-09-25 13:27:38 +0900

    整形のみ(マイナーチェンジ)

--- js/jquery.highlight-5.js
+++ js/jquery.highlight-5.js
@@ -1,25 +1,25 @@
 jQuery.fn.highlight = function(pat,rumortext,rumornum,teiseinum,correction,yuusendo) {
 	function innerHighlight(node, pat) {
-	var skip = 0;
-	if(node.nodeType == 3) {
-		var pos = node.data.toUpperCase().indexOf(pat);
-		pos -= (node.data.substr(0, pos).toUpperCase().length - node.data.substr(0, pos).length);
+		var skip = 0;
+		if(node.nodeType == 3) {
+			var pos = node.data.toUpperCase().indexOf(pat);
+			pos -= (node.data.substr(0, pos).toUpperCase().length - node.data.substr(0, pos).length);
 		if (pos >= 0) {
-		var spannode = document.createElement('rumorinfo');
-		spannode.className = 'rumorhighlight';
-		spannode.setAttribute('data-teiseinum', teiseinum);
-		spannode.setAttribute('data-rumortext', rumortext);
-		spannode.setAttribute('data-rumornum', rumornum);
-		spannode.setAttribute('data-correction', correction);
-		spannode.setAttribute('data-yuusendo', yuusendo);
-		var middlebit = node.splitText(pos);
-		var endbit = middlebit.splitText(pat.length);
-		var middleclone = middlebit.cloneNode(true);
-		spannode.appendChild(middleclone);
-		middlebit.parentNode.replaceChild(spannode, middlebit);
-		skip = 1;
+			var spannode = document.createElement('rumorinfo');
+			spannode.className = 'rumorhighlight';
+			spannode.setAttribute('data-teiseinum', teiseinum);
+			spannode.setAttribute('data-rumortext', rumortext);
+			spannode.setAttribute('data-rumornum', rumornum);
+			spannode.setAttribute('data-correction', correction);
+			spannode.setAttribute('data-yuusendo', yuusendo);
+			var middlebit = node.splitText(pos);
+			var endbit = middlebit.splitText(pat.length);
+			var middleclone = middlebit.cloneNode(true);
+			spannode.appendChild(middleclone);
+			middlebit.parentNode.replaceChild(spannode, middlebit);
+			skip = 1;
+		}
 	}
-}
   else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
    for (var i = 0; i < node.childNodes.length; ++i) {
     i += innerHighlight(node.childNodes[i], pat);
