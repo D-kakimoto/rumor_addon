dad47f09621894d743e802019c3b19ed79a7c4c4
dkakimoto <shinra.dai0419@gmail.com>
2017-11-13 19:04:29 +0900

    レーベンシュタイン関数を整形

--- js/highlight.js
+++ js/highlight.js
@@ -1,4 +1,4 @@
-//タグ全削除
+//htmlタグの除去
 function removeTag(str, arrowTag) {
     // 配列形式の場合は'|'で結合
     if ((Array.isArray ?
@@ -14,7 +14,7 @@ function removeTag(str, arrowTag) {
     return str.replace(pattern,'');
 }
 
-//レーベンシュタイン距離で計算
+//search関数(レーベンシュタイン距離)
 function search_custom(text,words){
   var checked;
   var findtext = new Array();
@@ -51,23 +51,23 @@ function search_custom(text,words){
   );
 }
 
-//ここはコピペ．呼び出すだけ
-function levenshteinDistance( str1, str2 ) {
-    var x = str1.length;
-    var y = str2.length;
-    var d = [];
-    for( var i = 0; i <= x; i++ ) {
+//レーベンシュタイン距離
+function levenshteinDistance(str1,str2){
+    var x=str1.length;
+    var y=str2.length;
+    var d=[];
+    for(var i=0; i<=x; i++){
         d[i] = [];
         d[i][0] = i;
     }
-    for( var i = 0; i <= y; i++ ) {
+    for(var i=0; i<=y; i++){
         d[0][i] = i;
     }
     var cost = 0;
-    for( var i = 1; i <= x; i++ ) {
-        for( var j = 1; j <= y; j++ ) {
-            cost = str1[i - 1] == str2[j - 1] ? 0 : 1;
-            d[i][j] = Math.min( d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost );
+    for(var i=1; i<=x; i++){
+        for(var j=1; j<=y; j++){
+            cost = str1[i-1] == str2[j-1] ? 0:1;
+            d[i][j] = Math.min(d[i-1][j]+1, d[i][j-1]+1, d[i-1][j-1] + cost);
         }
     }
     return d[x][y];
