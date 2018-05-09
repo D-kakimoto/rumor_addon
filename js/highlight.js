c16976eea26f850d22a0424de7c39449a500bc09
dkakimoto <shinra.dai0419@gmail.com>
2018-05-09 20:32:36 +0900

    トースト用スクリプトを作成

--- js/highlight.js
+++ js/highlight.js
@@ -134,29 +134,3 @@ function search(text,words) {
     function(res){}
   );
 };
-
-//ページ読み込み時流言検出数をトーストで表示
-function toast_on(count,string){
-  $(document).ready(function() {
-    toastr.options = {
-      "closeButton": true,
-      "debug": false,
-      "newestOnTop": false,
-      "progressBar": false,
-      "positionClass": "toast-top-right",
-      "preventDuplicates": false,
-      "showDuration": "300",
-      "hideDuration": "1000",
-      "timeOut": "5000",
-      "extendedTimeOut": "1000",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut"
-    }
-    Command:toastr["success"](string, count+"件の流言を検出")
-    $('#linkButton').click(function() {
-      toastr.success('click');
-    });
-  });
-}
